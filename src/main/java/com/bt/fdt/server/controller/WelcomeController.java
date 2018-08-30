f8a129a716c986465acf121ddc4dc63cbd618080
rkranjan <riteshk.ranjan@gmail.com>
2018-08-30 18:11:03 +0530

    swagger added

--- src/main/java/com/bt/fdt/server/controller/WelcomeController.java
+++ src/main/java/com/bt/fdt/server/controller/WelcomeController.java
@@ -1,13 +1,24 @@
 package com.bt.fdt.server.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-	@GetMapping("/")
+	@GetMapping("/welcome")
 	public String welcome() {
-		return "Hurray!! fdt server is up and running";
+		return "Hurray!! fdt server is up and running. Access rest api documentation using </br><b>http://localhost:8080/swagger-ui.html#/</b>";
+
+	}
+
+	@RequestMapping("/")
+	void handleFoo(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/swagger-ui.html#/");
 	}
 }
